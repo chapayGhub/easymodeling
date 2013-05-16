df256a1f09d53d64f4758407b4d663169b36dd76
ZZ <xzrunner@gmail.com>
2013-05-16 09:19:35 +0800

    modify copyright

--- src/easymodeling/SettingViewDlg.cpp
+++ src/easymodeling/SettingViewDlg.cpp
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2013 XZRUNNER http://runnersoft.net
+* Copyright (c) 2013 Guang Zhu http://runnersoft.net
 *
 * This software is provided 'as-is', without any express or implied
 * warranty.  In no event will the authors be held liable for any damages
